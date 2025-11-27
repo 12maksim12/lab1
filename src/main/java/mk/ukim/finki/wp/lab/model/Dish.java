4d1e86c2f06101119408e4db4c7343999e09580b
Maksim <mkostovski12@gmail.com>
2025-11-27 18:45:52 +0100

    second lab

--- src/main/java/mk/ukim/finki/wp/lab/model/Dish.java
+++ src/main/java/mk/ukim/finki/wp/lab/model/Dish.java
@@ -2,12 +2,14 @@ package mk.ukim.finki.wp.lab.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class Dish {
     private static long counter = 6L;
-    private long id;
+
+    private Long id;
     private String dishId;
     private String name;
     private String cuisine;
@@ -21,7 +23,19 @@ public class Dish {
         this.preparationTime = preparationTime;
     }
 
-    public void setPreparation(int preparationTime) {
+    public Dish(Long id, String dishId, String name, String cuisine, int preparationTime) {
+        this.id = counter++;
+        this.dishId = dishId;
+        this.name = name;
+        this.cuisine = cuisine;
         this.preparationTime = preparationTime;
+    }
+
+    public int getPreparation() {
+        return preparationTime;
+    }
+
+    public void setPreparation(int preparation) {
+        this.preparationTime = preparation;
     }
 }
